ccd3c92bbf5aac9a089389251b0882a9af57f0b4
Gustavo <gustavo.carvalho@clicksoft.com.br>
2019-01-29 20:12:28 -0200

    update: ImageWithPlaceholder tests

--- src/components/UI/ImageWithPlaceholder/__tests__/ImageWithPlaceholder.test.tsx
+++ src/components/UI/ImageWithPlaceholder/__tests__/ImageWithPlaceholder.test.tsx
@@ -6,7 +6,7 @@ describe('ImageWithPlaceholder', () => {
   it('renders correctly', () => {
     const imgUri = 'https://facebook.github.io/react-native/img/header_logo.png'
     const imageWithPlaceholder = renderer
-      .create(<ImageWithPlaceholder imgUri={imgUri} />)
+      .create(<ImageWithPlaceholder source={{ uri: imgUri }} />)
       .toJSON()
     expect(imageWithPlaceholder).toMatchSnapshot()
   })
